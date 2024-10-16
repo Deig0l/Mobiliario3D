42242f3f93fdfdc4e0671de5d9533cb5a3730a8c
FatiMoon81 <fatimalunad@gmail.com>
2024-10-16 13:55:38 -0600

    Rotated mesa() with coordinates

--- Mobiliario3D.cpp
+++ Mobiliario3D.cpp
@@ -66,11 +66,10 @@ void displayMobiliario() {
 	monitor(0, 0, -2.0);
 	mouse(0,0,-1);
 	keyboard(0,0,-1);*/
-	//mesa(0.0, 0.0, 0.0);
-	//glRotatef(180.0, 0.0, 1.0, 0.0);
-	silla(0.0, 0.0 , 0.0);
+	mesa(0.0, 0.0, 0.0);
+	//silla(3.0, 0.0 , 0.0);
 		
-		glPopMatrix();
+	glPopMatrix();
 
 	glutSwapBuffers();
 }
@@ -325,15 +324,15 @@ void keyboard(float x, float y, float z) {
 }
 
 void mesa(float x, float y, float z) {
-	prisma(9.0, 0.25, 4.5, x + 0.0, y + 4.5, z - 0.35, GREY);  // Tablero de la mesa
+	prisma(9.0, 0.25, 4.5, x + 0.0, y + 4.5, z + 0.35, GREY);  // Tablero de la mesa
 
 
 	prisma(0.35, 0.8, 4.5, x + 0.3, y + 0.0, z + 0.0, BLACK); // Pata acostada izquierda 
 	prisma(0.35, 0.8, 4.5, x + 8.4, y + 0.0, z + 0.0, BLACK); // Pata acostada derecha 
-	prisma(0.35, 4.5, 0.8, x + 0.3, y + 0.0, z - 0.35, DGREY); // Pata izquierda 
-	prisma(0.35, 4.5, 0.8, x + 8.4, y + 0.0, z - 0.35, DGREY); // Pata derecha 
+	prisma(0.35, 3.7, 0.8, x + 0.3, y + 0.8, z - 3.35, DGREY); // Pata izquierda 
+	prisma(0.35, 3.7, 0.8, x + 8.4, y + 0.8, z - 3.35, DGREY); // Pata derecha 
 
-	prisma(8.5, 2.3, 0.1, x + 0.25, y +2.3, z - 0.4, BLACK); // Rectangulo negro
+	prisma(7.95, 2.2, 0.1, x + 0.65, y +2.3, z - 4.05, BLACK); // Rectangulo negro
 
 }
 
